1697a0e37b3154e8ba913406d700d15909d003a4
isaias_pruebas <isgb_codigo@hotmail.com>
2024-06-23 21:19:34 -0600

    debug

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,37 +1,73 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { readProducts } from "../redux/productsSlice";
+import { createProduct, readProducts } from "../redux/productsSlice";
 
-const ProductsList = () =>{
+const ProductsList = () => {
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
 
-    const products = useSelector(state => state.products);
-    const dispatch = useDispatch();
+  const [newProductName, setNewProductName] = useState("");
+  const [editedProduct, setEditedProduct] = useState(null);
 
-    const [newProductName, setNewProductName] = useState("")
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/products")
+      .then((res) => {
+        console.log(res);
+        dispatch(readProducts(res.data));
+      })
+      .catch((err) => console.error(err));
+  }, [dispatch]);
 
-    useEffect(() => {
-        axios.get("http://localhost:3001/products").then((res) => {
-            console.log(res);
-            dispatch(readProducts(res.data))
-        }).catch(err => console.error(er))
-    }, [dispatch])
+  const handleCreateProduct = () => {
+    if (newProductName) {
+      const newProduct = { id: Date.now(), name: newProductName };
+      dispatch(createProduct(newProduct));
 
-    const handleCreateProduct = () => {}
-    const handleUpdateProduct = () => {}
-    const handleDeleteProduct = () => {}
+      axios
+        .post("http://localhost:3001/products", newProduct)
+        .then(() => {
+          setNewProductName;
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+  const handleUpdateProduct = () => {};
+  const handleDeleteProduct = () => {};
 
-    return <>
-        <h2>CRUD de Productos</h2>
-        <h3>Lista de Productos</h3>
-        <ul>
-            {products.data.map((product) => (<li key={product.id}>{product.name}</li>))}
-        </ul>
-        <aside>
-            <input type="text" value={newProductName} onChange={(e) => setNewProductName(e.target.value)}/>
-            <button onClick={handleCreateProduct}>Agregar Producto</button>
-        </aside>
+  return (
+    <>
+      <h2>CRUD de Productos</h2>
+      <h3>Lista de Productos</h3>
+      <ul>
+        {products.data.map((product) => (
+          <li key={product.id}>
+            {editedProduct.id === product.id ? (
+              <div>
+                <input type="text" />
+                <button>Actualizar</button>
+              </div>
+            ) : (
+              <div>
+                <span>{product.name}</span>
+                <button>Editar</button>
+                <button>Eliminar</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+      <aside>
+        <input
+          type="text"
+          value={newProductName}
+          onChange={(e) => setNewProductName(e.target.value)}
+        />
+        <button onClick={handleCreateProduct}>Agregar Producto</button>
+      </aside>
     </>
-}
+  );
+};
 
 export default ProductsList;
